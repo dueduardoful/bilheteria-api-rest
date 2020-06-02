e7368fc1c42ab04ff1c2eb8acc43b8ca0465d121
dueduardoful <dueduardoful@gmail.com>
2020-06-02 09:02:43 -0300

    adaptando projeto para o front-end - CORS adicionado

--- src/main/java/com/apirest/apirest/controllers/PedidoController.java
+++ src/main/java/com/apirest/apirest/controllers/PedidoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import com.apirest.apirest.entidades.Evento;
 import com.apirest.apirest.entidades.Pedido;
 import com.apirest.apirest.repository.EventoRepository;
 import com.apirest.apirest.repository.PedidoRepository;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/")
 public class PedidoController {
